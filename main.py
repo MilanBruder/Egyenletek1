5c2a96899e54139a935c565a07db56c56ae7c060
helembaiadam <74782835+helembaiadam@users.noreply.github.com>
2021-11-11 09:29:36 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-import math, cmath
- 
-a= input('Kérem a másodfokú egyenlet főegyütthatóját: ')
-a= float(a)
-while a==0:
-    print('Ez nem lesz másodfokú egyenlet; nem oldom meg.')
-    a= input('Kérem a másodfokú egyenlet főegyütthatóját: ')
-    a= float(a)
- 
-b= input('Kérem az elsőfokú tag együtthatóját: ')
-c= input('Kérem a konstans tagot: ')
-b= float(b)
-c= float(c)
- 
-d= b*b-4*a*c
-print('A diszkrimináns értéke', d)
- 
-if d>=0:
-    print('Van valós megoldás.')
-    x1= (-b-math.sqrt(d))/(2*a)
-    x2= (-b+math.sqrt(d))/(2*a)
-    print('Az egyik megoldás', x1)
-    print('A másik megoldás', x2)
-else:
-    print('Nincs valós megoldás.')
-    x1= (-b-cmath.sqrt(d))/(2*a)
-    x2= (-b+cmath.sqrt(d))/(2*a)
-    print('Az egyik megoldás', x1)
-    print('A másik megoldás', x2)
+import math
+
+
+
+def f1(label):
+    print(label)
+    txt = "\tVan-e megoldása a másodfokú egyenletnek?"
+    print(txt)
+    a = int(input("Kérem az a együtthatót: ") )
+    b = int(input("Kérem a b együtthatót: ") )
+    c = int(input("Kérem a c együtthatót: ") )
+
+    diszkriminans = b*b - 4*a*c
+
+    if (diszkriminans == 0):
+        txt = "\tAz egyenletnek egy megoldása van. "
+        print(txt)
+    else:
+        if (diszkriminans < 0):
+            txt = "\tAz egyenletnek nincs megoldása. "
+            print(txt)
+        else:
+            txt = "\tAz egyenletnek két megoldása van. "
+            print(txt)
+
+
+
+
+
+
+
+f1("1.feladat")
